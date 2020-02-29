8dda37211151edd14c1918ea6a04e325c3f80570
vierifath <61290164+vierifath@users.noreply.github.com>
2020-02-29 16:38:50 +0700

    Create soal2c.sh

---
+++ soal2/soal2c.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash
+
+for input in $@; do
+
+savehour=$(date -r $input +"%H")
+
+
+hurufkapital="ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
+hurufkecil="abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+
+string="${input%.*}"
+
+encript=$(echo $string | tr "${hurufkecil:0:26}${hurufkapital:0:26}" "${hurufkecil:${savehour}:26}${hurufkapital:${savehour}:26}")
+
+mv $string.txt $encript.txt
+
+done
