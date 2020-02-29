92908eb58bb5e15e16dd0a253611c48aff098ed4
vierifath <61290164+vierifath@users.noreply.github.com>
2020-02-29 17:17:33 +0700

    Create soalno2d.sh

---
+++ soal2/soalno2d.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for input in $@; do
+
+savehour=$(date -r $input +"%H")
+
+
+dcrpt=$(($savehour*(-1)+26))
+
+hurufkapital=ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ
+hurufkecil=abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz
+
+
+string="${input%.*}"
+
+decript=$(echo $string | tr "${hurufkecil:0:26}" "${hurufkecil:${dcrpt}:26}" | tr "${hurufkapital:0:26}" "${hurufkapital:${dcrpt}:26}") 
+
+mv $string.txt $decript.txt
+done
+
+
