20bce0ae67c968d65bec4b2ad908ace8e755cb20
vierifath <61290164+vierifath@users.noreply.github.com>
2020-02-29 16:03:38 +0700

    Update soal1Bshift.sh

--- soal1/soal1Bshift.sh
+++ soal1/soal1Bshift.sh
@@ -6,6 +6,6 @@ echo " 2 State  yang  memiliki profit paling sedikit  :"
 awk -F '	' '{if ($13 == "Central") i[$11]+=$21}
 END {for(jawaban in i) 
         print i[jawaban]" "jawaban} 
-' Sample-Superstore.tsv | sort -nr | tail -2
+' Sample-Superstore.tsv | sort -g | head -2
 
 
