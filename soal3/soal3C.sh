ca1ff5bfbf67de36bbaf7aad4439631b05801f67
vierifath <61290164+vierifath@users.noreply.github.com>
2020-02-29 21:34:11 +0700

    Create soal3C.sh

---
+++ soal3/soal3C.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+grep "location" wget.log > location.log
+save arr < location.log
+for ((i=0; i<28; i++)) do
+        for ((j=0; j<=i; j++))do
+
+if [ $i == $j ] then
+continue
+
+
+
+elif [ "${arr[$i]}" == "${arr[$j]}" ] then mv pdkt_kusuma_"$(($i+1))".jpg ./duplicate/duplicate_"$i".jpg
+fi
+done
+done
+
+for ((i=1; i<=28; i++)) do mv pdkt_kusuma_"$i".jpg ./kenangan/kenangan_"$i".jpg
